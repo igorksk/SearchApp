9c152dd170cb033d467dda97540334ac802378ef
Igor Kaseko <igorksk@gmail.com>
2024-08-05 20:09:12 +0300

    Fix backend internal port

--- frontend/search-app/src/api/personService.tsx
+++ frontend/search-app/src/api/personService.tsx
@@ -2,16 +2,18 @@ import axios from 'axios';
 import { Person } from '../data/Models';
 import { apiPort } from '../data/Constants';
 
-const baseUrl = `http://localhost:${apiPort}/people`; // Replace with your actual API base URL
+const api = axios.create({
+    baseURL: `http://localhost:${apiPort}`, // Replace with your API endpoint
+  });// Replace with your actual API base URL
 
 const personService = {
     getAllPersons: async (): Promise<Person[]> => {
-        const response = await axios.get<Person[]>(baseUrl);
+        const response = await api.get<Person[]>(`/people`);
         return response.data;
     },
 
     getPersonsByName: async (name: string): Promise<Person[]> => {
-        const response = await axios.get<Person[]>(`${baseUrl}/${name}`);
+        const response = await api.get<Person[]>(`/people/${name}`);
         return response.data;
     },
 };
