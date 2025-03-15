ebecb097988b366867f6ec2254ed4bd989c19973
Igor Kaseko <igorksk@gmail.com>
2025-03-15 15:17:54 +0200

    More real data

--- backend/SearchApi/SearchApi/Data/PeopleDataSeeder.cs
+++ backend/SearchApi/SearchApi/Data/PeopleDataSeeder.cs
@@ -12,44 +12,68 @@ namespace SearchApi.Data
         }
 
         public void Seed()
-        {
+{
             if (!_context.People.Any())
             {
                 _context.People.AddRange(
                     new Person
                     {
                         Id = 1,
-                        Name = "Alex",
-                        Phone = "+38098022",
-                        Skills = ["Dev", "DevOps"],
+                        Name = "Elena Petrova",
+                        Phone = "+380671234567",
+                        Skills = new List<string> { "Software Development", "C#", ".NET", "SQL" },
                         Jobs = new List<Job>
                         {
-                            new Job { Position = "Dev", Years = 1 },
-                            new Job { Position = "DevOps", Years = 2 }
+                            new Job { Position = "Junior Developer", Years = 2 },
+                            new Job { Position = "Software Developer", Years = 3 }
                         }
                     },
                     new Person
                     {
                         Id = 2,
-                        Name = "Margo",
-                        Phone = "+11111111",
-                        Skills = ["Designer", "MotionDesigner"],
+                        Name = "Dmytro Kovalenko",
+                        Phone = "+380509876543",
+                        Skills = new List<string> { "Project Management", "Agile", "Scrum", "Jira" },
                         Jobs = new List<Job>
                         {
-                            new Job { Position = "Dev", Years = 1 },
-                            new Job { Position = "DevOps", Years = 2 }
+                            new Job { Position = "Project Coordinator", Years = 4 },
+                            new Job { Position = "Project Manager", Years = 2 }
                         }
                     },
                     new Person
                     {
                         Id = 3,
-                        Name = "Sasha",
-                        Phone = "+22222222",
-                        Skills = ["Dev", "DevOps"],
+                        Name = "Olga Sydorenko",
+                        Phone = "+380932345678",
+                        Skills = new List<string> { "UX Design", "UI Design", "Figma", "Adobe XD" },
                         Jobs = new List<Job>
                         {
-                            new Job { Position = "Dev", Years = 1 },
-                            new Job { Position = "DevOps", Years = 2 }
+                            new Job { Position = "Junior Designer", Years = 1 },
+                            new Job { Position = "UX/UI Designer", Years = 3 }
+                        }
+                    },
+                    new Person
+                    {
+                        Id = 4,
+                        Name = "Andriy Melnyk",
+                        Phone = "+380958765432",
+                        Skills = new List<string> { "Data Analysis", "Python", "Pandas", "Machine Learning" },
+                        Jobs = new List<Job>
+                        {
+                            new Job { Position = "Data Analyst", Years = 3 },
+                            new Job { Position = "Data Scientist", Years = 1 }
+                        }
+                    },
+                    new Person
+                    {
+                        Id = 5,
+                        Name = "Iryna Kravchenko",
+                        Phone = "+380633456789",
+                        Skills = new List<string> { "Marketing", "Social Media", "SEO", "Content Creation" },
+                        Jobs = new List<Job>
+                        {
+                            new Job { Position = "Marketing Assistant", Years = 2 },
+                            new Job { Position = "Marketing Specialist", Years = 2 }
                         }
                     }
                 );
